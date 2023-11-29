b372e195223109e56c04b512d6ac56d707c7c646
Alexander Jurk <alexander.jurk@outlook.com>
2023-11-29 11:32:26 +0100

    Extended admin seeding

--- src/WishlistApp/Extensions/WebApplicationExtensions.cs
+++ src/WishlistApp/Extensions/WebApplicationExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 
 namespace WishlistApp.Extensions;
 
@@ -18,12 +19,17 @@ internal static class WebApplicationExtensions
 
         var username = app.Configuration.GetValue<string>("AdminUser:Username") ?? throw new ArgumentNullException("username", "Admin user name is needed");
         var password = app.Configuration.GetValue<string>("AdminUser:Password") ?? throw new ArgumentNullException("password", "Admin password is needed");
+        var expectedRoles = app.Configuration.GetSection("AdminUser:Roles").Get<string[]>() ?? [];
+        var expectedClaims = app.Configuration.GetSection("AdminUser:Claims").Get<Dictionary<string, string>>() ?? [];
 
-        var adminRole = await roleManager.FindByNameAsync("Admin");
-        if (adminRole is null)
+        foreach (var expectedRole in expectedRoles)
         {
-            adminRole = new() { Name = "Admin", };
-            await roleManager.CreateAsync(adminRole);
+            var role = await roleManager.FindByNameAsync(expectedRole);
+            if (role is null)
+            {
+                role = new() { Name = expectedRole };
+                await roleManager.CreateAsync(role);
+            }
         }
 
         var adminUser = await userManager.FindByNameAsync(username);
@@ -39,8 +45,15 @@ internal static class WebApplicationExtensions
 
             await userManager.CreateAsync(adminUser);
             await userManager.AddPasswordAsync(adminUser, password);
-            await userManager.AddToRoleAsync(adminUser, "Admin");
         }
+
+        var existingRoles = await userManager.GetRolesAsync(adminUser);
+        await userManager.RemoveFromRolesAsync(adminUser, existingRoles);
+        await userManager.AddToRolesAsync(adminUser, expectedRoles);
+
+        var existingClaims = await userManager.GetClaimsAsync(adminUser);
+        await userManager.RemoveClaimsAsync(adminUser, existingClaims);
+        await userManager.AddClaimsAsync(adminUser, expectedClaims.Select(kvp => new Claim(kvp.Key, kvp.Value)));
 
         return app;
     }
