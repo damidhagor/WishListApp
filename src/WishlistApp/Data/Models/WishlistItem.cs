ba1c1b05b117646d78442cd1dc78a7b28b94b077
Alexander Jurk <alexander.jurk@outlook.com>
2023-12-07 01:03:12 +0100

    Add WishlistItem Order

--- src/WishlistApp/Data/Models/WishlistItem.cs
+++ src/WishlistApp/Data/Models/WishlistItem.cs
@@ -10,6 +10,8 @@ public sealed class WishlistItem
 
     public required string Url { get; init; }
 
+    public int Order { get; init; } = int.MaxValue;
+
     public int? BoughtByWishlistShareId { get; set; }
 
     public WishlistShare? BoughtByWishlistShare { get; set; }
