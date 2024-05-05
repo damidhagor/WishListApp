8d7164b35c3c0543f884c03db901537a55a50d24
Alexander Jurk <alexander.jurk@outlook.com>
2024-05-05 16:47:27 +0200

    Remove Cli and Sql dependencies

--- src/WishListApp.Cli/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.CommandLine;
-using WishListApp.Cli.Database;
-
-var rootCommand = new RootCommand("Cli tool for the WishList App");
-
-rootCommand.AddDatabaseCommand();
-
-await rootCommand.InvokeAsync(args);
