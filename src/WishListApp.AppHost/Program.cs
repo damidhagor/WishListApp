f94070ec6c368bf98756a0425825743892e155be
Alexander Jurk <alexander.jurk@outlook.com>
2024-05-31 15:10:09 +0200

    Adjusted helm charts

--- src/WishListApp.AppHost/Program.cs
+++ src/WishListApp.AppHost/Program.cs
@@ -3,10 +3,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 var mongoDB = builder
     .AddMongoDB("mongodb")
     .WithDataVolume("mongodb-volume")
+    .WithOtlpExporter()
     .WithMongoExpress();
 
 var wishListApp = builder
     .AddProject<Projects.WishListApp>("wishlistapp")
-    .WithReference(mongoDB);
+    .WithReference(mongoDB, "MongoDB");
 
 builder.Build().Run();
--- src/WishListApp.Data/ServiceCollectionExtensions.cs
+++ src/WishListApp.Data/ServiceCollectionExtensions.cs
@@ -6,7 +6,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddWishListData(this IServiceCollection services, IConfiguration configuration, Action<MongoClientSettings>? configure = null)
     {
-        var connectionString = configuration.GetConnectionString("mongodb");
+        var connectionString = configuration.GetConnectionString("MongoDB");
         var mongoSettings = MongoClientSettings.FromConnectionString(connectionString);
 
         mongoSettings.ClusterConfigurator = clusterBuilder => clusterBuilder.Subscribe(new DiagnosticsActivityEventSubscriber(
