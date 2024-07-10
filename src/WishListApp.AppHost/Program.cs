7b8eb0bfc3371540dfc34c2d951c4cb52ac212e3
Alexander Jurk <alexander.jurk@outlook.com>
2024-07-10 15:52:49 +0300

    Add inital working Keycloak resource

--- src/WishListApp.AppHost/Program.cs
+++ src/WishListApp.AppHost/Program.cs
@@ -10,4 +10,24 @@ var wishListApp = builder
     .AddProject<Projects.WishListApp>("wishlistapp")
     .WithReference(mongoDB, "MongoDB");
 
+builder.AddContainer("keycloak", "quay.io/keycloak/keycloak", "25.0.1")
+    .WithEndpoint(
+        port: 8080,
+        targetPort: 8080,
+        scheme: "http",
+        name: "web")
+    .WithEndpoint(
+        port: 9000,
+        targetPort: 9000,
+        scheme: "http",
+        name: "management")
+    .WithEnvironment("KEYCLOAK_ADMIN", "admin")
+    .WithEnvironment("KEYCLOAK_ADMIN_PASSWORD", "admin")
+    .WithBindMount(
+        "Resources/wishlist-dev-realm.json",
+        "/opt/keycloak/data/import/wishlist-dev-realm.json")
+    .WithArgs(
+        "start-dev",
+        "--import-realm");
+
 builder.Build().Run();
