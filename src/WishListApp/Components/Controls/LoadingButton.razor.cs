82ad115a4717741a5e74fda3f22d154276c9fe77
Alexander Jurk <alexander.jurk@outlook.com>
2024-08-11 15:06:22 +0200

    Add LoadingButton

---
+++ src/WishListApp/Components/Controls/LoadingButton.razor.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components;
+
+namespace WishListApp.Components.Controls;
+
+public partial class LoadingButton
+{
+    private bool _isLoading = false;
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public Func<Task>? LoadingAction { get; set; }
+
+    private async Task OnClick()
+    {
+        try
+        {
+            _isLoading = true;
+
+            if (LoadingAction is not null)
+            {
+                await LoadingAction();
+            }
+        }
+        finally
+        {
+            _isLoading = false;
+        }
+    }
+}
