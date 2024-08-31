6cd8eb9b58c2e958bba0ed58236c7712a0687fb2
Alexander Jurk <alexander.jurk@outlook.com>
2024-08-31 17:24:50 +0200

    Adjusted IdentityHeader layout

---
+++ src/WishListApp/Identity/Components/IdentityHeader.razor.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using WishListApp.Identity.Services;
+
+namespace WishListApp.Identity.Components;
+
+public partial class IdentityHeader(IStringLocalizer<Localization> localizer, IIdentityService accountService)
+{
+    private readonly IStringLocalizer<Localization> _localizer = localizer;
+    private readonly IIdentityService _accountService = accountService;
+
+    [Parameter]
+    public string? Class { get; set; }
+
+    private static string GetUsername(AuthenticationState authenticationState)
+    {
+        var user = authenticationState.User;
+
+        var nameClaim = user.Claims.FirstOrDefault(c => c.Type == "name");
+        return nameClaim?.Value ?? user.Identity?.Name ?? "";
+    }
+}
