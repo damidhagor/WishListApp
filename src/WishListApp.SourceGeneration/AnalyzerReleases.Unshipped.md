7f3ab840dbd69ef9dadb671650fa9d99f282a80f
Alexander Jurk <alexander.jurk@outlook.com>
2024-09-09 23:35:17 +0200

    Add localization resource analyzer

---
+++ src/WishListApp.SourceGeneration/AnalyzerReleases.Unshipped.md
@@ -0,0 +1,10 @@
+﻿; Unshipped analyzer release
+; https://github.com/dotnet/roslyn-analyzers/blob/main/src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md
+
+### New Rules
+
+Rule ID | Category | Severity | Notes
+--------|----------|----------|-------
+LR0001 | Resource | Warning | LocalizationResourceAnalyzer
+LR0002 | Resource | Error | LocalizationResourceAnalyzer
+LR0003 | Resource | Warning | LocalizationResourceAnalyzer
---
+++ src/WishListApp.SourceGeneration/LocalizationResourceAnalyzer/Models/Resource.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace WishListApp.SourceGeneration.LocalizationResourceAnalyzer.Models;
+
+internal sealed record Resource(
+    string FullPath,
+    string Path,
+    string Filename,
+    string BaseFilename,
+    AdditionalText Text)
+{
+    public Resource(string fullPath, AdditionalText text)
+        : this("", "", "", "", text)
+    {
+        FullPath = fullPath;
+        Path = System.IO.Path.GetDirectoryName(fullPath) ?? throw new InvalidOperationException();
+        Filename = System.IO.Path.GetFileName(fullPath);
+
+        var dotIndex = Filename.IndexOf('.');
+        BaseFilename = dotIndex == -1 ? Filename : Filename.Substring(0, dotIndex);
+    }
+}
